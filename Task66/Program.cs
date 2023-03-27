234c31e45f1d126f58ab2ac1d6fae8a45e68637d
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-03-27 19:30:06 +0300

    Added Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Num 66
+// задать значения M и N;
+// найти сумму натуральных элементов в промежутке от M до N;
+// M = 1, N = 15 -> 120
+// M = 4, N = 8 -> 30
+
+int Num(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int M, int N)
+{
+    if (M == N) return N;
+    if (M < N)
+    {
+        return (M + Sum(M + 1, N));
+    }
+    else
+    {
+        return (M + Sum(M - 1, N));
+    }
+}
+
+Console.WriteLine(Sum(Num("Enter initial value (M): "), Num("Enter end value (N): ")));
+
